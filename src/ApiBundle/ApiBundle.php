8b9a1e421cdfb5016985b51036a01e6475ccfda4
Ian <bayn151@gmail.com>
2025-01-06 02:22:25 +1000

    Basic on-premises deployment

---
+++ src/ApiBundle/ApiBundle.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace App\ApiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ApiBundle extends Bundle
+{
+
+}
---
+++ src/ApiBundle/Controller/PaymentController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\ApiBundle\Controller;
+
+class PaymentController extends BaseController
+{
+    public function calculatePrice()
+    {
+
+    }
+
+    public function purchase()
+    {
+
+    }
+}
